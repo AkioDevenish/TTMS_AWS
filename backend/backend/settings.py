cff8ca0efa3d508978ca38f0375ea253b4f7b3ea
Christopher Parks <cba.parks@gmail.com>
2025-01-10 01:09:03 -0400

    settings and requirements updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,8 +63,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOWED_ORIGINS = [
-     "http://localhost:3000",
-       "http://localhost:5173",
+	"http://localhost:3000",
+    "http://localhost:5173",
+    "http://167.88.45.83"
    ]
 
 
