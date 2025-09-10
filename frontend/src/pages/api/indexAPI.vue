1167bcd0b90024eb4b6413887456388ad6d25cb4
Christopher Parks <cba.parks@gmail.com>
2025-09-10 18:35:52 -0400

    chore: use slim class on tables

--- frontend/src/pages/api/indexAPI.vue
+++ frontend/src/pages/api/indexAPI.vue
@@ -1,25 +1,31 @@
 <template>
-    <div class="container-fluid api-accounts-overview">
-        <div class="row">
-            <Card3 colClass="col-sm-12 " title="API Accounts Overview" headerTitle="true" cardhaderClass="title-header"
-                text="true" :desc="desc">
-                <template v-if="isAdmin">
-                  <SupportTable />
-                </template>
-                <template v-else>
-                  <div class="alert alert-warning mt-4">You do not have permission to view API key management.</div>
-                </template>
-            </Card3>
-        </div>
+  <div class="container-fluid api-accounts-overview">
+    <div class="row">
+      <Card3
+          cardClass="slim"
+          colClass="col-sm-12 "
+          title="API Accounts Overview"
+          headerTitle="true"
+          cardhaderClass="title-header"
+          text="true"
+          :desc="desc">
+        <template v-if="isAdmin">
+          <SupportTable />
+        </template>
+        <template v-else>
+          <div class="alert alert-warning mt-4">You do not have permission to view API key management.</div>
+        </template>
+      </Card3>
     </div>
+  </div>
 </template>
 <script lang="ts" setup>
 import { ref, defineAsyncComponent } from 'vue';
 import { useAuthStore } from '@/store/auth';
-const Card3 = defineAsyncComponent(() => import("@/components/common/card/CardData3.vue"))
-// const SupportTicket = defineAsyncComponent(() => import("@/components/theme/api/SupportTicket.vue"))
-const SupportTable = defineAsyncComponent(() => import("@/components/theme/api/SupportTable.vue"))
-const authStore = useAuthStore()
-const { isAdmin } = authStore
-let desc = ref<string>("List of Accounts and API Status Of Customers")
+
+const Card3 = defineAsyncComponent(() => import('@/components/common/card/CardData3.vue'));
+const SupportTable = defineAsyncComponent(() => import('@/components/theme/api/SupportTable.vue'));
+const authStore = useAuthStore();
+const { isAdmin } = authStore;
+let desc = ref<string>('List of Accounts and API Status Of Customers');
 </script>
--- frontend/src/pages/api/indexApiKeyUsageLogs.vue
+++ frontend/src/pages/api/indexApiKeyUsageLogs.vue
@@ -1,28 +1,35 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <Card3 colClass="col-sm-12" title="API Key Usage Logs" headerTitle="true" cardhaderClass="title-header" text="true">
-                <!-- The API Key Usage Log Table Component will go here -->
-                <!-- We will pass the api_key_uuid to it as a prop -->
-                 <ApiKeyUsageLogTable :apiKeyUuid="apiKeyUuid" />
-            </Card3>
-        </div>
+  <div class="container-fluid">
+    <div class="row">
+      <Card3
+          cardClass="slim"
+          colClass="col-sm-12"
+          title="API Key Usage Logs"
+          headerTitle="true"
+          cardhaderClass="title-header"
+          text="true">
+        <!-- The API Key Usage Log Table Component will go here -->
+        <!-- We will pass the api_key_uuid to it as a prop -->
+        <ApiKeyUsageLogTable :apiKeyUuid="apiKeyUuid" />
+      </Card3>
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, defineAsyncComponent, onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
-const Card3 = defineAsyncComponent(() => import("@/components/common/card/CardData3.vue"));
+
+const Card3 = defineAsyncComponent(() => import('@/components/common/card/CardData3.vue'));
 // We will define and import the ApiKeyUsageLogTable component next
-const ApiKeyUsageLogTable = defineAsyncComponent(() => import("@/components/theme/api_management/ApiKeyUsageLogTable.vue"));
+const ApiKeyUsageLogTable = defineAsyncComponent(() => import('@/components/theme/api_management/ApiKeyUsageLogTable.vue'));
 
 const route = useRoute();
 const apiKeyUuid = ref<string | string[] | null>(null);
 
 // Watch for changes in the route parameters to get the api_key_uuid
 watch(() => route.params.uuid, (newUuid) => {
-    apiKeyUuid.value = newUuid;
+  apiKeyUuid.value = newUuid;
 }, { immediate: true }); // Set initial value on mount
 
-</script> 
+</script>
